a802cdb5aacb099bd65234185deb58eab824ff11
Fabio Cotrim <fabiohenrique_p13@hotmail.com>
2023-06-13 12:09:52 -0300

    copy method

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,36 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Program {
+	/*fazer um método que copia os elementos de uma lista para uma
+	outra lista que pode ser mais genérica que a primeira.*/
 
 	public static void main(String[] args) {
 
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
 		List<Object> myObjs = new ArrayList<Object>();
-		myObjs.add("Maria");
-		myObjs.add("Alex");
-		
-		List<? super Number> myNums = myObjs;
-		
-		myNums.add(10);
-		myNums.add(3.14);
-		
-		Number x = myNums.get(0); // erro de compilacao
 
+		copy(myInts, myObjs);
+		printList(myObjs);
+		copy(myDoubles, myObjs);
+		printList(myObjs);
+	}
+
+	public static void copy(List<? extends Number> source, List<? super Number> destination) {
+		for (Number number : source) {
+			destination.add(number);
+		}
+	}
+
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.print(obj + " ");
+		}
+		System.out.println();
 	}
 
 }
